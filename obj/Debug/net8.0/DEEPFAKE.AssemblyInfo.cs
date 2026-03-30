e4820a2fa736f8019cc031d8f183987fe7b34989
dhruvajudiya24 <dhruvajudiya522@gmail.com>
2026-03-30 22:46:35 +0530

    final working version

--- obj/Debug/net8.0/DEEPFAKE.AssemblyInfo.cs
+++ obj/Debug/net8.0/DEEPFAKE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEEPFAKE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3742c72cb3be49e86cf31a73fc8125889a78fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578a6142eb2fb20d7bb89e14a941514a9e5bb22a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEEPFAKE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEEPFAKE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
