4a1f973cb71249088abbdd10e74a13679ae2c114
dhruvajudiya24 <dhruvajudiya522@gmail.com>
2026-03-31 09:35:26 +0530

    fix: hardcode ollama ngrok url

--- obj/Debug/net8.0/DEEPFAKE.AssemblyInfo.cs
+++ obj/Debug/net8.0/DEEPFAKE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEEPFAKE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477b345aa460af9a0fb49f25ebebda5dbbbf3df9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb8061264a394d3f4a82cf081556fa973582773")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEEPFAKE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEEPFAKE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
